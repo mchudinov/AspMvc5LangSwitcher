9ce0f88b473b0e9812261b520df85329a65e8514
Mikael Chudinov <mikael@chudinov.net>
2017-03-27 11:36:47 +0200

    Initial commit.

---
+++ AspMvc5LangSwitcher/Controllers/DefaultController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace AspMvc5LangSwitcher.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
